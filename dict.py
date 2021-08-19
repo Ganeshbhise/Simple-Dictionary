8908a674f6e2438fb0f656b3a65ea4f383f716a8
Ganesh Natha bhise <87429129+Ganeshbhise@users.noreply.github.com>
2021-08-19 16:32:04 +0530

    Create dict.py
    this is a simple dictionary by which we can access the meaning of the respective  word

---
+++ dict.py
@@ -0,0 +1,4 @@
+dict={"joy":"happy","sad":"sorrow","good":"best","hard":"difficult"}
+print("Enter the word of which u want to know the meaning")
+a=input()
+print("the meaning of entered word is:",dict.get(a))
